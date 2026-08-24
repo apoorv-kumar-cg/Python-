b590822e0cad8eac63f643e60324804135e19174
apoorv-kumar-cg <apoorvkumar.cg@gmail.com>
2026-08-24 11:09:17 +0530

    added

--- class.py
+++ class.py
@@ -8,3 +8,15 @@ student_age = 19
 print(student_name)
 print(student_age)
 
+
+age_int=18
+height_floate=5.8
+name_str="rahul"
+Age_bool=True
+city_nonetype=None
+
+print(type(age_int))
+print(type(height_floate))
+print(type(name_str))
+print(type(Age_bool))
+print(type(city_nonetype))
