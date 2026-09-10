0cc9def0be19ee9ce497df86ba54182731472ad2
apoorv-kumar-cg <apoorvkumar.cg@gmail.com>
2026-09-10 09:32:25 +0530

    loop added

--- Python If-Else Problem Solving/question 23.py
+++ Python If-Else Problem Solving/question 23.py
@@ -1,9 +1,8 @@
 a=input("Username=")
 b=input("Password=")
 
-if a=="admin":
-    if b=="python123":
-        print("Login successful")
+if a=="admin" and  b=="python123":
+    print("Login successful")
 elif a!="admin":
     print("User not found")
 elif b!="python123":
--- Python If-Else Problem Solving/question 24.py
+++ Python If-Else Problem Solving/question 24.py
@@ -1,22 +1,27 @@
 a=int(input("Original Price=="))
 
 if a<500:
+    print(f"original amount={a}")
     print("discount=0%")
     print(f"Discount amount={0*a}")
-    print(f"Final amount={(0*a)+a}")
+    print(f"Final amount={a-(0*a)}")
 elif a<999:
+    print(f"original amount={a}")
     print("discount=5%")
-    print(f"Discount amount={5*a}")
-    print(f"Final amount={(5*a)+a}")
+    print(f"Discount amount={.05*a}")
+    print(f"Final amount={a-(.05*a)}")
 elif a<1999:
+    print(f"original amount={a}")
     print("discount=10%")
-    print(f"Discount amount={10*a}")
-    print(f"Final amount={(10*a)+a}")
-elif a<4999:
+    print(f"Discount amount={.10*a}")
+    print(f"Final amount={a-(.10*a)}")
+elif a<=4999:
+    print(f"original amount={a}")
     print("discount=15%")
-    print(f"Discount amount={15*a}")
-    print(f"Final amount={(15*a)+a}")
-elif a>5000:
+    print(f"Discount amount={.15*a}")
+    print(f"Final amount={a-(.15*a)}")
+elif a>=5000:
+    print(f"original amount={a}")
     print("discount=20%")
-    print(f"Discount amount={20*a}")
-    print(f"Final amount={(20*a)+a}")
+    print(f"Discount amount={.20*a}")
+    print(f"Final amount={a-(.20*a)}")
